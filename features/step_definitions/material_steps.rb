cde854e067e738cd73e3738ffa5bb3ed1037afb7
Anderson Gonçalves <Andersontno@gmail.com>
2018-12-04 22:16:28 -0300

    Implementação de todos os testes e Postmortem

--- features/step_definitions/material_steps.rb
+++ features/step_definitions/material_steps.rb
@@ -21,13 +21,12 @@ Given("Eu abro a pagina de informacoes dos materiais") do
   expect(page).to have_content('Materials')
 end
 
-When("Eu seleciono a opcao de remover o material com nome {string}") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+When("Eu seleciono a opcao de remover o material com nome {string}") do |nome|
+  click_link "delete-#{nome}"
 end
 
-Then("Eu vejo uma mensagem informando que o material com o nome {string} foi removido") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
-end
+Then("Eu vejo uma mensagem informando que o material com o nome {string} foi removido") do |nome|
+  expect(page).to have_content(nome)end
 
 When("Eu seleciono o material com nome {string}") do |string|
   pending # Write code here that turns the phrase above into concrete actions
@@ -37,20 +36,21 @@ Then("Eu sou direcionado para uma pagina que mostra todos os dados do material d
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-When("Eu seleciono a opcao de editar o material com nome {string}") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+When("Eu seleciono a opcao de editar o material com nome {string}") do |nome|
+  click_link "editar-#{nome}"
 end
 
-When("Eu edito o nome do material {string} para {string}") do |string, string2|
-  pending # Write code here that turns the phrase above into concrete actions
+When("Eu edito o nome do material {string} para {string}") do |string, nome|
+  fill_in 'material[nome]', with:nome
+  click_button 'Update Material'
 end
 
-Then("Eu vejo uma mensagem informando que o material com o nome {string} foi editado com sucesso") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+Then("Eu vejo uma mensagem informando que o material com o nome {string} foi editado com sucesso") do |nome|
+  expect(page).to have_content(nome)
 end
 
 Then("Eu vejo uma mensagem informando erro no cadastro do material com o nome {string}") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(page).to have_content('error')
 end
 
 When("Eu seleciono a opcao de editar o material com nome {string} e valor {string}") do |string, string2|
@@ -73,3 +73,15 @@ When("Eu altero o campo de valor para {string}") do |string|
   pending # Write code here that turns the phrase above into concrete actions
 end
 
+
+And("existe um material com o nome {string}, valor {string}, tipo {string}, cor {string} e origem {string}") do |nome, valor, tipo, cor, origem|
+  caminhoAntigo = URI.parse(current_url)
+  visit "/materials/new"
+  fill_in 'material[nome]', with:nome
+  fill_in 'material[valor]', with:valor
+  fill_in 'material[tipo]', with:tipo
+  fill_in 'material[cor]', with:cor
+  fill_in 'material[origem]', with:origem
+  click_button 'Create Material'
+  visit caminhoAntigo
+end
