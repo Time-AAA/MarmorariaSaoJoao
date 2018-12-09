685c4b4cd3191d811a495cbfb6fd26c572b61b03
Armstrong Lohãns <lohansdemelo1108@hotmail.com>
2018-12-09 19:37:01 -0300

    RSPEC

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :materials
 
   root 'welcome#index'
+
+  # match 'coverage/' => 'coverage#index.html', via: :get
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,7 @@
-require 'simplecov'
-SimpleCov.start
-
-# Previous content of test helper now starts here
-
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
